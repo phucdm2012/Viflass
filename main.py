d1c7ff6cfac5f29c04a7754216b4b784d620a051
Dương Minh Phúc <phucdm2012@gmail.com>
2026-03-15 20:23:59 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/download", methods=["GET"])
+def download():
+    return render_template("download.html")
+
+
+@app.route("/relese", methods=["GET"])
+def relese():
+    return render_template("relese.html")
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
